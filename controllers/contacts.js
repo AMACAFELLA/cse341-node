0c99d8d774281b04cf71500290e509d7a033117d
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2023-01-12 15:47:19 +0200

    CRUD added

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,54 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favouriteColor: req.body.favouriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if( response.acknowledged ) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'An error occurred while creating the contact')
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favouriteColor: req.body.favouriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: userId }, contact);
+  if( response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'AN error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: 'Contact not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'An error occurred while deleting the contact.', error: err });
+  }
+};
+
+
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
